1314d0ea722b100a7820784bae46d113aa3e227c
EAlbee <EAlbee@madisoncollege.edu>
2018-10-09 21:20:26 -0500

    delete first component
    not needed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { TjsAddNumbersComponent } from './tjs-add-numbers/tjs-add-numbers.component';
-import { TjsNumberAdditionComponent } from './tjs-number-addition/tjs-number-addition.component';
 import { TjsNumberAdditionEAComponent } from './tjs-number-addition-ea/tjs-number-addition-ea.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TjsAddNumbersComponent,
-    TjsNumberAdditionComponent,
     TjsNumberAdditionEAComponent
   ],
   imports: [
